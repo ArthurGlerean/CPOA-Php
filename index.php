08c28685cf93f173c3b0ab92422f07cc1fde5cc9
Arthur GLEREAN <arthur.glerean@gmail.com>
2022-05-24 09:22:25 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    $_SESSION["isConnected"] =(isset($_SESSION["isConnected"])) ? $_SESSION["isConnected"] : 2 ;    //si l'utilisateur s'est déjà connecté (connecté=1;déconnecté=0) alors il prend sa valeur sinon 2.
+    $target = (isset($_GET["target"])) ? $_GET["target"] : "home"  ;      //si target= existe dans l'addresse alors prend la valeur de target sinon "home"
+    if(file_exists("controleurs/c_".$target.".php")){   
+        require_once("controleurs/c_".$target.".php");
+    }else{
+        echo "ERROR 404";
+    }
+?>
---
+++ lib/connectDb.php
@@ -0,0 +1,11 @@
+<?php
+    $bdd = oci_new_connect('p2006969','584758','(
+        Description = (
+            address_list = 
+            (address = (protocol = tcp)(host = iutdoua-ora.univ-lyon1.fr)(port = 1521))
+        )
+        (connect_data =
+             (Service_name = ORCL.UNIV-LYON1.FR)
+        )
+    )');
+?>
